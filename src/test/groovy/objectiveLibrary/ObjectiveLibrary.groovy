3068c96458fba302002fa4201c4d579883bc4950
gregor <grzegorz@passfailerror.com>
2022-11-26 18:37:03 +0100

    Add objective library example

---
+++ src/test/groovy/objectiveLibrary/ObjectiveLibrary.groovy
@@ -0,0 +1,45 @@
+package objectiveLibrary
+
+class ObjectiveLibrary implements Serializable {
+
+    private static final long serialVersionUID
+
+    def scriptObject // org.jenkinsci.plugins.workflow.cps.CpsScript
+    def paramsMap
+    def utils
+
+    ObjectiveLibrary(scriptObject, paramsMap) {
+        this.scriptObject = scriptObject
+        this.paramsMap = paramsMap
+        this.utils = new ObjectiveUtils(scriptObject)
+    }
+
+    def initialize() {
+        checkParamsMap()
+    }
+
+    def checkParamsMap() {
+        paramsMap.each { entry ->
+            utils.checkNotNull(entry.key, entry.value)
+        }
+    }
+
+    def run() {
+        firstStage()
+        secondStage()
+    }
+
+    def firstStage() {
+        scriptObject.stage('First stage') {
+            scriptObject.echo "I am working in first stage"
+            scriptObject.echo "paramsMap p1: "+paramsMap.p1
+        }
+    }
+
+    def secondStage() {
+        scriptObject.stage('Second stage') {
+            scriptObject.echo "I am working in second stage"
+            scriptObject.echo "paramsMap p2: "+paramsMap.p2
+        }
+    }
+}
---
+++ src/test/groovy/objectiveLibrary/ObjectiveUtils.groovy
@@ -0,0 +1,23 @@
+package objectiveLibrary
+
+class ObjectiveUtils implements Serializable{
+
+    private static final long serialVersionUID
+
+    def scriptObject
+
+    ObjectiveUtils(scriptObject){
+        this.scriptObject = scriptObject
+    }
+
+    def checkNotNull(String param, String value){
+        scriptObject.echo("check not null: "+param)
+        if(value != null){
+            scriptObject.echo(param + "==" + value)
+        }
+        else{
+            scriptObject.error(param+" is null")
+        }
+    }
+
+}
