e7fdf09f9726be6ff4e400b47b741b68a2f19318
gregor <grzegorz@passfailerror.com>
2022-11-28 16:46:16 +0100

    Remove redundancy in testObjectiveLibrary while still keeping it DAMP

--- src/test/groovy/acceptance/testObjectiveLibrary.groovy
+++ src/test/groovy/acceptance/testObjectiveLibrary.groovy
@@ -9,53 +9,42 @@ import static com.passfailerror.assertion.GeneralAssertion.step
 class testObjectiveLibrary extends GroovyTestCase {
 
     Jenkinson jenkinson
+    def paramsMap
+    ObjectiveLibrary objectiveLibrary
 
     void setUp() {
-        if (jenkinson == null) {
-            jenkinson = Jenkinson.initialize()
-        }
+        jenkinson = Jenkinson.initialize()
+        paramsMap = ["p1": "p1_value", "p2": "p2_value"]
+        objectiveLibrary = new ObjectiveLibrary(jenkinson.getPipelineScript(), paramsMap)
+        objectiveLibrary.initialize()
     }
 
     void test_objectiveLibraryAssertion_returnsTrue_forExistingInvocation() {
-        //GIVEN
-        def paramsMap = ["p1": "p1_value", "p2": "p2_value"]
-        ObjectiveLibrary objectiveLibrary = new ObjectiveLibrary(jenkinson.getPipelineScript(), paramsMap)
         //WHEN
-        objectiveLibrary.initialize()
         objectiveLibrary.run()
         //THEN
-        assert step("echo","I am working in first stage").isCalled()
+        assert step("echo", "I am working in first stage").isCalled()
     }
 
     void test_objectiveLibraryAssertion_returnsFalse_forUnexistingValueInvocation() {
-        //GIVEN
-        def paramsMap = ["p1": "p1_value", "p2": "p2_value"]
-        ObjectiveLibrary objectiveLibrary = new ObjectiveLibrary(jenkinson.getPipelineScript(), paramsMap)
         //WHEN
-        objectiveLibrary.initialize()
         objectiveLibrary.run()
         //THEN
-        assert step("echo","Unexisting text").isCalled() == false
+        assert step("echo", "Unexisting text").isCalled() == false
     }
 
     void test_objectiveLibraryAssertion_returnsFalse_forUnexistingStepInvocation() {
-        //GIVEN
-        def paramsMap = ["p1": "p1_value", "p2": "p2_value"]
-        ObjectiveLibrary objectiveLibrary = new ObjectiveLibrary(jenkinson.getPipelineScript(), paramsMap)
         //WHEN
-        objectiveLibrary.initialize()
         objectiveLibrary.run()
         //THEN
-        assert step("unexisting step","I am working in first stage").isCalled() == false
+        assert step("unexisting step", "I am working in first stage").isCalled() == false
     }
 
     void test_objectiveLibraryAssertion_returnsFalse_forStepWhichExistsInNotExecutedPartOfLibrary() {
-        //GIVEN
-        def paramsMap = ["p1": "p1_value", "p2": "p2_value"]
-        ObjectiveLibrary objectiveLibrary = new ObjectiveLibrary(jenkinson.getPipelineScript(), paramsMap)
         //WHEN
         objectiveLibrary.secondStage()
         //THEN
-        assert step("echo","I am working in first stage").isCalled() == false
+        assert step("echo", "I am working in first stage").isCalled() == false
     }
+
 }
