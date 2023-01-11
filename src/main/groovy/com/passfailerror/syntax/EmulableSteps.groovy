dc90630daf036c32da6e1dc9cd7527a058c16d5a
gregor <grzegorz@passfailerror.com>
2023-01-11 22:39:49 +0100

    Implement real command execution

--- src/main/groovy/com/passfailerror/syntax/EmulableSteps.groovy
+++ src/main/groovy/com/passfailerror/syntax/EmulableSteps.groovy
@@ -18,16 +18,55 @@ class EmulableSteps extends EmulableToken {
             pipelineScript.metaClass."$currentStep" = { Object... params ->
                 log.info(currentStep + " " + params[0].toString())
                 def actualCommand
+                def commandOutput
                 if (params[0] instanceof Map) {
                     actualCommand = params[0]["script"]
                 } else {
                     actualCommand = params[0]
                 }
                 if (shouldBeExecuted(currentStep, actualCommand)) {
-                    throw new UnsupportedOperationException("step calling not implemented yet")
+                    commandOutput = shExecute(actualCommand)
                 }
                 resultStackProcessor.storeInvocation(currentStep, params, pipelineScript.getBinding().getVariables())
+                return commandOutput
             }
+        }
+    }
+
+    def shExecute(actualCommand) {
+        def commandParams = actualCommand.split(" ")
+        try {
+            def stdOut = new StringBuilder()
+            def stdErr = new StringBuilder()
+            def proc = executeCommand(commandParams)
+            proc.waitForProcessOutput(stdOut, stdErr)
+            proc.waitForOrKill(5000)
+            def combinedOutput = stdOut.toString() + stdErr.toString()
+            if (proc.exitValue() != 0) { // command was run and decision is made basing on its exit value
+                throw new RuntimeException(combinedOutput)
+            }
+            log.debug(combinedOutput)
+            return combinedOutput
+        }
+        catch (Exception e) {// command was not run at all
+            throw new RuntimeException(e)
+        }
+    }
+
+    def executeCommand(commandParams) {
+        def execPrefix = getOsBasedExecutionPrefix()
+        def params = []
+        params.addAll(execPrefix)
+        params.addAll(commandParams)
+        return new ProcessBuilder(params).start()
+    }
+
+    def getOsBasedExecutionPrefix(){
+        if (System.getProperty("os.name").contains("Windows")){
+            return ["cmd.exe", "/c"]
+        }
+        else{
+            return ["bash", "-c"]
         }
     }
 }
