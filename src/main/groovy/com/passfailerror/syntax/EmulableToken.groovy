810807f824f16124a0f609c17d647312cea8e73b
gregor <grzegorz@passfailerror.com>
2023-01-14 21:44:48 +0100

    #1 Simplify EmulableToken

--- src/main/groovy/com/passfailerror/syntax/EmulableToken.groovy
+++ src/main/groovy/com/passfailerror/syntax/EmulableToken.groovy
@@ -1,39 +1,15 @@
 package com.passfailerror.syntax
 
 import com.passfailerror.resultStack.ResultStackProcessor
+import groovy.util.logging.Slf4j
 
+@Slf4j
 class EmulableToken {
 
     static ResultStackProcessor resultStackProcessor
 
     Map<String,List<String>> tokenParamValueMap = [:]
-
-    Map<String,String> realExecutionMap = [:]
-    Map<String,Class> emulatorMap = [:]
-    Map<String,String> returnValueMap = [:]
-
-    def mock(pipelineScript) {}
-
-    def shouldBeExecuted(token, command) {
-        if (tokenMapContains(tokenParamValueMap, token, command) && realExecutionMap.containsKey(token) ) {
-            return true
-        }
-        return false
-    }
-
-    def shouldBeEmulated(token, command) {
-        if (tokenMapContains(tokenParamValueMap, token, command) && emulatorMap.containsKey(token)) {
-            return true
-        }
-        return false
-    }
-
-    def shouldReturnValue(token, command){
-        if (tokenMapContains(tokenParamValueMap, token, command) && returnValueMap.containsKey(token)) {
-            return true
-        }
-        return false
-    }
+    Map<String,Object> actionMap = [:]
 
     def tokenMapContains(map, token, command){
         if (map.containsKey(token) && command.contains(map[token])) {
@@ -41,6 +17,8 @@ class EmulableToken {
         }
         return false
     }
+
+    def modifyCommandOutput(){}
 
     def execute(actualCommand) {
         def commandParams = actualCommand.split(" ")
