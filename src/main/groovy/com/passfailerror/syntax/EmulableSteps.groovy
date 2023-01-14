17e7cc824f04f846488a8e51cf5cd540d87e313c
gregor <grzegorz@passfailerror.com>
2023-01-14 20:03:39 +0100

    #1 Rename shExecute to execute and move it to EmulableToken

--- src/main/groovy/com/passfailerror/syntax/EmulableSteps.groovy
+++ src/main/groovy/com/passfailerror/syntax/EmulableSteps.groovy
@@ -46,7 +46,7 @@ class EmulableSteps extends EmulableToken {
                     actualCommand = params[0]
                 }
                 if (shouldBeExecuted(currentStep, actualCommand)) {
-                    commandOutput = shExecute(actualCommand)
+                    commandOutput = execute(actualCommand)
                 }
                 resultStackProcessor.storeInvocation(currentStep, params, pipelineScript.getBinding().getVariables())
                 return commandOutput
@@ -73,26 +73,6 @@ class EmulableSteps extends EmulableToken {
                 resultStackProcessor.storeInvocation(currentStep, params, pipelineScript.getBinding().getVariables())
                 return commandOutput
             }
-        }
-    }
-
-    def shExecute(actualCommand) {
-        def commandParams = actualCommand.split(" ")
-        try {
-            def stdOut = new StringBuilder()
-            def stdErr = new StringBuilder()
-            def proc = executeCommand(commandParams)
-            proc.waitForProcessOutput(stdOut, stdErr)
-            proc.waitForOrKill(5000)
-            def combinedOutput = stdOut.toString() + stdErr.toString()
-            if (proc.exitValue() != 0) { // command was run and decision is made basing on its exit value
-                throw new RuntimeException(combinedOutput)
-            }
-            log.debug(combinedOutput)
-            return combinedOutput
-        }
-        catch (Exception e) {// command was not run at all
-            throw new RuntimeException(e)
         }
     }
 
--- src/main/groovy/com/passfailerror/syntax/EmulableToken.groovy
+++ src/main/groovy/com/passfailerror/syntax/EmulableToken.groovy
@@ -41,4 +41,24 @@ class EmulableToken {
         }
         return false
     }
+
+    def execute(actualCommand) {
+        def commandParams = actualCommand.split(" ")
+        try {
+            def stdOut = new StringBuilder()
+            def stdErr = new StringBuilder()
+            def proc = executeCommand(commandParams)
+            proc.waitForProcessOutput(stdOut, stdErr)
+            proc.waitForOrKill(5000)
+            def combinedOutput = stdOut.toString() + stdErr.toString()
+            if (proc.exitValue() != 0) { // command was run and decision is made basing on its exit value
+                throw new RuntimeException(combinedOutput)
+            }
+            log.debug(combinedOutput)
+            return combinedOutput
+        }
+        catch (Exception e) {// command was not run at all
+            throw new RuntimeException(e)
+        }
+    }
 }
