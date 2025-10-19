87c043116831afcc6c376b6521ceee0fcf801252
grzegorzgrzegorz <grzegorz@passfailerror.com>
2025-10-19 18:31:53 +0200

    #16 Refactor ResultStack tests

--- src/test/groovy/resultStack/ResultStackValidatorTest.groovy
+++ src/test/groovy/resultStack/ResultStackValidatorTest.groovy
@@ -6,88 +6,88 @@ import groovy.test.GroovyTestCase
 class ResultStackValidatorTest extends GroovyTestCase {
 
 
-    void testUnexistingStageAssertion_returnsFalse_evenWhenStepExists() {
-        //GIVEN
+    void "test: unexisting stage assertion returns False even when step exists"() {
+        given:
         String scriptContent = 'echo "testing"'
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemCallsStepWithParam("unexisting stage", "echo", null) == false
     }
 
-    void testExistingStageAssertion_returnsTrue_onExistingStep() {
-        //GIVEN
+    void "test: existing stage assertion returns True on existing step"() {
+        given:
         String scriptContent = '''
             stage("test"){
                 echo "testing"
             }
             '''
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemCallsStepWithParam("test", "echo", null)
     }
 
-    void testExistingStageAssertion_returnsTrue_onExistingStepWithParam() {
-        //GIVEN
+    void "test: existing stage assertion returns True on existing step with parameter"() {
+        given:
         String scriptContent = '''
             stage("test"){
                 echo "testing"
             }
             '''
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemCallsStepWithParam("test", "echo", "testing")
     }
 
-    void testExistingStageAssertion_returnsFalse_onExistingStepWithUnexistingParam() {
-        //GIVEN
+    void "test: existing stage assertion returns False on existing step with unexisting parameter"() {
+        given:
         String scriptContent = '''
             stage("test"){
                 echo "testing"
             }
             '''
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemCallsStepWithParam("test", "echo", "testing2") == false
     }
 
-    void testExistingStageAssertion_returnsFalse_onUnexistingStepWithExistingParam() {
-        //GIVEN
+    void "test: existing stage assertion returns False on unexisting step with existing param"() {
+        given:
         String scriptContent = '''
             stage("test"){
                 echo "testing"
             }
             '''
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemCallsStepWithParam("test", "echo2", "testing") == false
     }
 
-    void testExistingStageAssertion_returnsFalse_onUnexistingStep() {
-        //GIVEN
+    void "test: existing stage assertion returns false on unexisting step"() {
+        given:
         String scriptContent = '''
             stage("test"){
                 echo "testing"
             }
             '''
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemCallsStepWithParam("test", "unexisitingStep", null) == false
     }
 
-    void testUnexistingStageAssertion_returnsFalse_onExistingEnvVariable() {
-        //GIVEN
+    void "test: unexisting stage assertion returns false on existing env variable"() {
+        given:
         String scriptContent = '''
             stage("test"){
                 echo "testing"
@@ -95,14 +95,14 @@ class ResultStackValidatorTest extends GroovyTestCase {
             }
             '''
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemHasEnvVariable("unexisting", "TEST1") == false
     }
 
-    void testExistingStageAssertion_returnsFalse_onUnexistingEnvVariable() {
-        //GIVEN
+    void "test: existing stage assertion returns false on unexisting env variable"() {
+        given:
         String scriptContent = '''
             stage("test"){
                 echo "testing"
@@ -110,14 +110,14 @@ class ResultStackValidatorTest extends GroovyTestCase {
             }
             '''
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemHasEnvVariable("test", "unexistingVariable") == false
     }
 
-    void testExistingStageAssertion_returnsTrue_onExistingEnvVariable() {
-        //GIVEN
+    void "test: existing stage assertion returns True on existing env variable"() {
+        given:
         String scriptContent = '''
             stage("test"){
                 echo "testing"
@@ -125,9 +125,9 @@ class ResultStackValidatorTest extends GroovyTestCase {
             }
             '''
         def jenkinson = Jenkinson.initializeFromText(scriptContent)
-        //WHEN
+        when:
         jenkinson.run()
-        //THEN
+        then:
         assert jenkinson.getResultStackValidator().declarativeItemHasEnvVariable("test", "TEST1")
     }
 }
